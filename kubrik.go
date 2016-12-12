c70113737acd4cb06fa1336db58eb86f70fa4be1
David J Felix <felix.davidj@gmail.com>
2016-12-11 22:10:17 -0500

    Begin using httprouter

--- kubrik.go
+++ kubrik.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/urfave/negroni"
 )
 
-func handle(w http.ResponseWriter, r *http.Request) {
+func login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "Hello")
 }
 
 func main() {
+	router := httprouter.New()
+	router.POST("/users/login", login)
 	n := negroni.Classic()
-	n.UseHandlerFunc(handle)
+	n.UseHandler(router)
 	n.Run(":8080")
 }
