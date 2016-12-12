c98014deb988b7da7f1c66d1536953f33ea2f878
David J Felix <felix.davidj@gmail.com>
2016-12-11 21:54:22 -0500

    Implement basic hello world

--- kubrik.go
+++ kubrik.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/urfave/negroni"
+)
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
+}
+
 func main() {
+	n := negroni.Classic()
+	n.UseHandlerFunc(handle)
+	n.Run(":8080")
 }
