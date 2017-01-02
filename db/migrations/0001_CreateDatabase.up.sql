65ccafe8a43f723571fba0d0f3bdabf3a31517c3
David J Felix <felix.davidj@gmail.com>
2017-01-01 20:56:41 -0500

    Update migrations and add new facebook_users table

--- db/migrations/0001_CreateDatabase.up.sql
+++ db/migrations/0001_CreateDatabase.up.sql
@@ -1,6 +1,6 @@
-CREATE EXTENSION "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS users (
 	id                 UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
 	username           VARCHAR(31) UNIQUE,
-	email              VARCHAR(255) UNIQUE,
+	email              VARCHAR(255) UNIQUE NOT NULL,
 	encrypted_password BYTEA);
---
+++ db/migrations/0002_CreateFacebookAuth.up.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS facebook_users (
+  facebook_user_id VARCHAR(32) PRIMARY KEY,
+  user_id UUID REFERENCES users(id) ON DELETE CASCADE UNIQUE NOT NULL);
