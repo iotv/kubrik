2983987dd7182854614376d7f80e667e9826daa4
David J Felix <felix.davidj@gmail.com>
2017-01-11 19:38:13 -0500

    Clean up code in api a bit

--- api/api.go
+++ api/api.go
@@ -26,8 +26,8 @@ func write400(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusBadRequest)
 	encoder.Encode(errorResponse{
 		HttpStatus: http.StatusBadRequest,
-		Message: "Bad request",
-		Errors: &[]errorStruct{},
+		Message:    "Bad request",
+		Errors:     &[]errorStruct{},
 	})
 }
 
@@ -38,7 +38,7 @@ func write401(w http.ResponseWriter, errs *[]errorStruct) {
 	encoder.Encode(&errorResponse{
 		HttpStatus: http.StatusUnauthorized,
 		Message:    "Unauthorized",
-		Errors: errs,
+		Errors:     errs,
 	})
 }
 
@@ -51,7 +51,7 @@ func write404(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusNotFound)
 	encoder.Encode(errorResponse{
 		HttpStatus: http.StatusNotFound,
-		Message: "Resource not found",
+		Message:    "Resource not found",
 		Errors: &[]errorStruct{
 			{
 				Error: "Id does not exist",
@@ -73,8 +73,8 @@ func write422(w http.ResponseWriter, errs *[]errorStruct) {
 	w.WriteHeader(http.StatusUnprocessableEntity)
 	encoder.Encode(errorResponse{
 		HttpStatus: http.StatusUnprocessableEntity,
-		Message: "Unprocessable entity. JSON was parsed but did not match the expected structure",
-		Errors: errs,
+		Message:    "Unprocessable entity. JSON was parsed but did not match the expected structure",
+		Errors:     errs,
 	})
 }
 
@@ -84,8 +84,8 @@ func write500(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusInternalServerError)
 	encoder.Encode(errorResponse{
 		HttpStatus: http.StatusInternalServerError,
-		Message: "Internal server error",
-		Errors: &[]errorStruct{},
+		Message:    "Internal server error",
+		Errors:     &[]errorStruct{},
 	})
 
 }
