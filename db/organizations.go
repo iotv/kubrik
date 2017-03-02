252cf28e6968859d76d911818257fccd6393aa79
David J Felix <felix.davidj@gmail.com>
2017-03-02 00:38:24 -0500

    Parse db response for insane join

--- db/organizations.go
+++ db/organizations.go
@@ -1,10 +1,24 @@
 package db
 
+type PermissionModel struct {
+	Id                 string
+	PermissionTypeId   string
+	PermissionTypeName string
+}
+
+type GroupModel struct {
+	Id          string
+	Name        string
+	IsPublic    bool
+	Permissions []PermissionModel
+}
+
 type OrganizationModel struct {
 	Id        string
 	Name      string
 	IsUserOrg bool
 	OwnerId   string
+	Groups    []GroupModel
 }
 
 type OrganizationGroupModel struct {
@@ -36,8 +50,7 @@ func CreateOrganization(name, ownerId string, isUserOrg bool) (*OrganizationMode
 }
 
 func GetOrganizationById(id string) (*OrganizationModel, error) {
-	const qs = "SELECT name, owner_id FROM organizations WHERE id=$1"
-	const q = `SELECT o.id, o.name, o.is_user_org, o.owner_id,
+	const qs = `SELECT o.name, o.is_user_org, o.owner_id,
   g.id as group_id, g.name as group_name, g.is_public as group_is_public,
   p.id as permission_id, p.permission_type_id,
   t.name as permission_type_name
@@ -49,24 +62,76 @@ FROM organizations o
   LEFT JOIN organization_group_permission_types t
     ON p.permission_type_id = t.id
 WHERE o.id = $1`
+
 	conn, err := PgPool.Acquire()
 	if err != nil {
 		return nil, err
 	}
 	defer PgPool.Release(conn)
 
-	var name string
-	var ownerId string
-	row := conn.QueryRow(qs, id)
-	err = row.Scan(&name, &ownerId)
+	rows, err := conn.Query(qs, id)
 	if err != nil {
 		return nil, err
 	}
-	return &OrganizationModel{
-		Id:      id,
-		Name:    name,
-		OwnerId: ownerId,
-	}, nil
+	defer rows.Close()
+
+	response := OrganizationModel{}
+	for rows.Next() {
+		var name string
+		var isUserOrg bool
+		var ownerId string
+		var groupId string
+		var groupName string
+		var groupIsPublic bool
+		var permissionId string
+		var permissionTypeId string
+		var permissionTypeName string
+
+		err = rows.Scan(
+			&name, &isUserOrg, &ownerId,
+			&groupId, &groupName, &groupIsPublic,
+			&permissionId, &permissionTypeId, &permissionTypeName)
+		if err != nil {
+			return nil, err
+		}
+
+		response.Id = id
+		response.Name = name
+		response.IsUserOrg = isUserOrg
+		response.OwnerId = ownerId
+
+		// Find group if it exists
+		groupExists := false
+		groupIndex := 0
+		for index, group := range response.Groups {
+			if group.Id == groupId {
+				groupExists = true
+				groupIndex = index
+				break
+			}
+		}
+
+		// Create group if it doesn't exist
+		if !groupExists && groupId != "" {
+			groupIndex = len(response.Groups)
+			response.Groups = append(response.Groups, GroupModel{
+				Id:          groupId,
+				Name:        groupName,
+				IsPublic:    groupIsPublic,
+				Permissions: []PermissionModel{},
+			})
+		}
+
+		// Create permissions on group
+		if permissionId != "" {
+			response.Groups[groupIndex].Permissions = append(response.Groups[groupIndex].Permissions, PermissionModel{
+				Id:                 permissionId,
+				PermissionTypeId:   permissionTypeId,
+				PermissionTypeName: permissionTypeName,
+			})
+		}
+	}
+	return &response, nil
 }
 
 func GetOrganizationByName(name string) (*OrganizationModel, error) {
